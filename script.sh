671edf86cb26216f75f867f75b80742cda34d7ea
gosht9 <gosht9@gmail.com>
2016-01-04 13:42:40 +0100

    inicio script

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
 sudo aptitude update
-sudo aptitude upgrade
-sudo aptitude install curl
-sudo su
-curl -sL https://deb.nodesource.com/setup | bash -
-sudo aptitude install nodejs
+sudo apt-get install nodejs
+sudo apt-get install npm
