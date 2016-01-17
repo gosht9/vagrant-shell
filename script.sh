037d4cc63253b8e3608f75d2264a7a5a71bee64e
Angel Manuel <amansp_85@outlook.com>
2016-01-17 12:03:34 +0100

    final result

--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
-sudo aptitude update
-sudo apt-get install nodejs
-sudo apt-get install npm
+sudo echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" > /etc/apt/source.list 
+sudo aptitude update && aptitude upgrade
+sudo aptitude install -y mongodb-org
+sudo curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo apt-get update
 sudo npm install -g express
